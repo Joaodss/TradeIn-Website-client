84917409690e2ff8a599a4e92069fa57f36b63ed
João Afonso Silva <joaoafonsodss@gmail.com>
2022-03-02 13:53:08 +0000

    initialize components

---
+++ src/app/modules/trade-in-request/components/product-element/product-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-element',
+  templateUrl: './product-element.component.html',
+  styleUrls: ['./product-element.component.css']
+})
+export class ProductElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/trade-in-request/components/product-page/product-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css']
+})
+export class ProductPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
